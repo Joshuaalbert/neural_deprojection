6f3df79793cfdb8162a9e6f8ee81c88e6e054f54
joshuaalbert <45720Physics>
2020-10-12 12:09:02 +0200

    * init

--- neural_deprojection/monet/download_data.sh
+++ neural_deprojection/monet/download_data.sh
@@ -1,4 +1,8 @@
 #!/usr/env bash
 
 kaggle competitions download -c gan-getting-started
+mkdir data
+mv gan-getting-started.zip data
+cd data
 unzip gan-getting-started.zip
+cd ..
---
+++ neural_deprojection/monet/run_training.py
@@ -0,0 +1,24 @@
+import argparse,  sys
+#imports
+
+
+def main(arg0, arg1, arg2):
+    print(arg0, arg1, arg2)
+
+
+def add_args(parser):
+    parser.register("type", "bool", lambda v: v.lower() == "true")
+    parser.add_argument('--arg0', help='Obs number L*',default=0, type=int, required=False)
+    parser.add_argument('--arg1', help='Obs number L*',default=0, type=int, required=False)
+    parser.add_argument('--arg2', help='Obs number L*',default=0, type=int, required=False)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Runs simple training loop.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    add_args(parser)
+    flags, unparsed = parser.parse_known_args()
+    print("Running with:")
+    for option, value in vars(flags).items():
+        print("    {} -> {}".format(option, value))
+    main(**vars(flags))
