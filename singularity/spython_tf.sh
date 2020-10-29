362d1e9d07125ab1d623ce072f75808137422b3f
joshuaalbert <albert@strw.leidenuniv.nl>
2020-10-29 14:16:19 +0100

    * let singularity use nvidia stuff

--- singularity/spython_tf.sh
+++ singularity/spython_tf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Ensure your repo is in '$HOME/git/'"
 export SINGULARITY_BINDPATH="/data"
-singularity exec $HOME/git/neural_deprojection/singularity/tensorflow_gpu.simg python "$@"
+singularity exec --nv $HOME/git/neural_deprojection/singularity/tensorflow_gpu.simg python "$@"
