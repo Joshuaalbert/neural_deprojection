42c4dd037303bd6e77cd8dfcdc7f399543a01086
joshuaalbert <albert@strw.leidenuniv.nl>
2020-10-29 14:10:16 +0100

    * bind data

--- singularity/spython_tf.sh
+++ singularity/spython_tf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Ensure your repo is in '$HOME/git/'"
+export SINGULARITY_BINDPATH="/data"
 singularity exec $HOME/git/neural_deprojection/singularity/tensorflow_gpu.simg python "$@"
