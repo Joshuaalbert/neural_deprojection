1234e663c569739f88c902e3ae1713ed72e0a646
Matthijs van Groeningen <matthijsvangroeningen@gmail.com>
2020-11-11 13:39:20 +0100

    This now works in the unedited pynbody package, also wrote some comments to make everything a little more clear

--- neural_deprojection/visualization/pynbody_and_mayavi_test.py
+++ neural_deprojection/visualization/pynbody_and_mayavi_test.py
@@ -2,11 +2,6 @@ from mayavi import mlab
 import pynbody as nb
 import numpy as np
 import h5py
-import pysph.tools.interpolator as interp
-from pysph.base.particle_array import ParticleArray
-from tvtk.util.ctf import PiecewiseFunction, ColorTransferFunction
-import pysph.base
-from mayavi.tools.pipeline import scalar_field
 import matplotlib.pyplot as plt
 
 my_file1 = '/home/matthijs/Documents/Studie/Master_Astronomy/1st_Research_Project/Data/Test/Clusterfind/Magneticum/Box2b_hr/snap_026/0/simcut/kurz0cv5y4ln1vmc/snap_026'
@@ -18,50 +13,49 @@ f1 = nb.load(my_file1)
 f2 = nb.load(my_file2)
 f3 = nb.load(my_file3)
 
+#Create a new 'SimSnap' object from an hdf5 file
 hdf = h5py.File(jul_file, 'r')
-
-print(type(f3))
 data = hdf['particles']['0000000001']['attributes']
-print(data['x'].attrs.get('units'))
+#print(list(data.keys()))
 f4 = nb.new(gas=9577)
-print(list(data.keys()))
-print('='*20)
 for name in list(data.keys()):
-    # print(data[name].attrs.get('units'))
     f4.gas[name] = data[name]
-f4.set_units_system(velocity='m s^-1', distance='m', mass='kg', temperature='K')
-print(list(f4.gas.all_keys()))
-# print(data['temp'][:10])
-print(data['u'][:10])
-print(f4.gas['smooth'][:10])
 
-# print(type(f1))
+# Not sure it's necessary to change the units, but here is code for it if needed
+# print(data['density'].attrs.get('units'))
+# f4.set_units_system(velocity='m s^-1', distance='m', mass='kg', temperature='K')
 
-f = f4
-# f.physical_units()
-sim = f.gas
+#This function changes the x, y and z coordinates so that they are centered around 0 and optionally offset from 0
+def localize(sim, offset = [0.,0.,0.]):
+    sim['x'] = (sim['x'] - (np.max(sim['x']) + np.min(sim['x'])) / 2) + offset[0]
+    sim['y'] = (sim['y'] - (np.max(sim['y']) + np.min(sim['y'])) / 2) + offset[1]
+    sim['z'] = sim['z'] - (np.max(sim['z']) + np.min(sim['z'])) / 2 + offset [2]
+
+sim = f4.gas
 print(list(sim.all_keys()))
 
-av_z = True
-
-qty1 = 'radius'
+localize(sim) #Magneticum snapshots are not centered around 0
+av_z = True #average the quantity down line of sight, if False a slice at z=0 is taken unless z != 0 in the offset
+qty1 = 'rho'
 qty2 = 'u'
 m = 1 #magnification
-o = [0., 0., 0.] #offset
 res = 500 #resolution
-filename = 'radius.png'
+width = np.max(sim['x']) - np.min(sim['x']) #width of the image without magnification
+filename = 'picture_file.png' #filename to store pictures, use 'filename' keyword in image function
 
-# volume1 = nb.plot.sph.volume(f4.gas, resolution=200, qty = qty1, log = True, dynamic_range = 10)
-# # volume2 = nb.plot.sph.volume(f.gas, resolution=200, qty = qty2)
-#
-max_x = np.max(sim['x'])
-min_x = np.min(sim['x'])
-print(max_x, min_x)
+# MAYAVI PLOTTING
 
-img1 = nb.plot.sph.image(sim, qty=qty1, av_z=av_z, width=max_x - min_x, resolution=res, magnification=m, offset=o, filename = filename)
-# img2 = nb.plot.sph.velocity_image(sim, width=max_x - min_x, magnification=m, filename = filename, quiverkey = False)
-# img3 = nb.plot.sph.contour(sim, qty1, width=max_x - min_x, resolution=res, av_z=av_z, levels = 20, filename = filename)
+#I can't get this to work for the hdf5 file yet, interpolating gives a grid of zeros
 
-plt.show()
+# volume1 = nb.plot.sph.volume(sim, resolution=200, qty = qty1, log = True, dynamic_range = 10)
+# volume2 = nb.plot.sph.volume(sim, resolution=200, qty = qty2)
 
 # mlab.show()
+
+# PYNBODY PLOTTING
+
+# img1 = nb.plot.sph.image(sim, qty=qty1, width=width/m, av_z=av_z, resolution=res)
+# img2 = nb.plot.sph.velocity_image(sim, width=width/m, filename = filename, quiverkey = False)
+img3 = nb.plot.sph.contour(sim, qty1, width=width/m, resolution=res, av_z=av_z, levels = 20)
+
+plt.show()
