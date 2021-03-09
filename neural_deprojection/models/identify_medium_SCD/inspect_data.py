a945d3c65869ea46a961c2401d3b394fdc29c78c
JuliusHendrix <jlam.hendrix@gmail.com>
2021-03-09 19:29:49 +0100

    inspect data on ALICE

--- neural_deprojection/models/identify_medium_SCD/inspect_data.py
+++ neural_deprojection/models/identify_medium_SCD/inspect_data.py
@@ -9,7 +9,7 @@ from graph_nets.graphs import GraphsTuple
 from graph_nets.utils_np import graphs_tuple_to_networkxs, networkxs_to_graphs_tuple, get_graph
 import numpy as np
 import networkx as nx
-from networkx.drawing import draw
+from networkx.drawing import draw, draw_networkx_edges, draw_networkx_nodes
 from tqdm import tqdm
 from scipy.optimize import bisect
 from scipy.spatial.ckdtree import cKDTree
@@ -49,7 +49,8 @@ def decode_examples(record_bytes, node_shape=None, edge_shape=None, image_shape=
         # Schema
         dict(
             image=tf.io.FixedLenFeature([], dtype=tf.string),
-            example_idx=tf.io.FixedLenFeature([], dtype=tf.string),
+            snapshot=tf.io.FixedLenFeature([], dtype=tf.string),
+            projection=tf.io.FixedLenFeature([], dtype=tf.string),
             **feature_to_graph_tuple('graph')
         )
     )
@@ -58,8 +59,10 @@ def decode_examples(record_bytes, node_shape=None, edge_shape=None, image_shape=
     # image = tf.math.log(image)
     # image = (image - tf.reduce_mean(image, axis=None)) / std(image, axis=None)
     image.set_shape(image_shape)
-    example_idx = tf.io.parse_tensor(parsed_example['example_idx'], tf.int32)
-    example_idx.set_shape(())
+    snapshot = tf.io.parse_tensor(parsed_example['snapshot'], tf.int32)
+    snapshot.set_shape(())
+    projection = tf.io.parse_tensor(parsed_example['projection'], tf.int32)
+    projection.set_shape(())
     graph_nodes = tf.io.parse_tensor(parsed_example['graph_nodes'], tf.float32)
     # graph_nodes = (graph_nodes - tf.reduce_mean(graph_nodes, axis=0)) / std(graph_nodes, axis=0)
     if node_shape is not None:
@@ -78,7 +81,9 @@ def decode_examples(record_bytes, node_shape=None, edge_shape=None, image_shape=
                         senders=senders,
                         n_node=tf.shape(graph_nodes)[0:1],
                         n_edge=tf.shape(graph_edges)[0:1])
-    return (graph, image, example_idx)
+
+    print('image ', image)
+    return (graph, image, snapshot, projection)
 
 
 def _get_data(dir):
@@ -178,7 +183,7 @@ def get_data_image(data_dirs):
 
 
 def plot_from_ds_item(ds_item):
-    (graph, image, e_idx) = ds_item
+    (graph, image, snapshot, projection) = ds_item
     nx_graph = nx.DiGraph()
 
     pos = dict()  # for plotting node positions.
@@ -206,118 +211,164 @@ def plot_from_ds_item(ds_item):
 
     print('len(pos) = {}\nlen(edgelist) = {}'.format(len(pos), len(edgelist)))
 
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(60, 20))
+    # fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(60, 20))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(40, 20))
+    fig.suptitle(f'snapshot: {snapshot}, projection: {projection}', size=40)
     ax1.set_aspect(1)
     ax2.set_aspect(1)
-    ax3.set_aspect(1)
+    # ax3.set_aspect(1)
 
-    draw(nx_graph, ax=ax1, pos=pos,
-         edge_color='red', node_size=10, width=0.1, arrowstyle='-',
+    draw_networkx_nodes(nx_graph, ax=ax1, pos=pos, node_size=10,
          node_color=dens_list, cmap='viridis')
 
-    ax2.imshow(image.numpy()[:, :, 0])
+    draw_networkx_edges(nx_graph, ax=ax1, pos=pos,
+         edge_color='grey', width=0.1, arrowstyle='-', alpha=1)
+
+    # draw(nx_graph, ax=ax1, pos=pos,
+    #      edge_color='red', node_size=10, width=0.1, arrowstyle='-',
+    #      node_color=dens_list, cmap='viridis', alpha=0.3)
+
+    ax2.imshow(image.numpy()[:, :, 0].T, origin='lower')
     # ax2.set_xlim(0.7,1.3)
     # ax2.set_ylim(0.7,1.3)
     ax2.axis('off')
 
-    _x = np.linspace(np.min(x_list), np.max(x_list), 256)
-    _y = np.linspace(np.min(y_list), np.max(y_list), 256)
-    _z = np.linspace(np.min(z_list), np.max(z_list), 256)
-    x, y, z = np.meshgrid(_x, _y, _z, indexing='ij')
+    # _x = np.linspace(np.min(x_list), np.max(x_list), 700)
+    # _y = np.linspace(np.min(y_list), np.max(y_list), 700)
+    # _z = np.linspace(np.min(z_list), np.max(z_list), 700)
+    # x, y, z = np.meshgrid(_x, _y, _z, indexing='ij')
+    #
+    # interp = interpolate.griddata((x_list, y_list, z_list),
+    #                               dens_list,
+    #                               xi=(x, y, z), fill_value=np.min(dens_list))
+    #
+    # im = np.sum(interp, axis=2)
+    # # im = interp[:,:,350]
+    # im = np.trapz(interp, axis=2)
 
-    interp = interpolate.griddata((x_list, y_list, z_list),
-                                  dens_list,
-                                  xi=(x, y, z), fill_value=0.0)
+    # H, xedges, yedges = np.histogram2d(x_list, y_list, bins=200, weights=dens_list, range=[[-0.5, 4], [-2,2]])
+    # print(H)
+    # ax3.imshow(H.T, origin='lower', interpolation='bilinear')    #, extent=(xmin, xmax, ymin, ymax))
 
-    im = np.sum(interp, axis=2)
-    ax3.imshow(im.T, origin='lower', extent=(xmin, xmax, ymin, ymax))
-    ax3.axis('off')
 
     image_dir = '/data2/hendrix/images_tf/'
-    mp_lock.acquire()  # make sure no duplicate files are made / replaced
-    graph_image_idx = len(glob.glob(os.path.join(image_dir, 'graph_image_*')))
-    mp_lock.release()  # make sure no duplicate files are made / replaced
+    # mp_lock.acquire()  # make sure no duplicate files are made / replaced
+    # graph_image_idx = len(glob.glob(os.path.join(image_dir, 'graph_image_*')))
+    # mp_lock.release()  # make sure no duplicate files are made / replaced
 
-    plt.savefig(os.path.join(image_dir, 'graph_image_{}'.format(graph_image_idx)))
+    plt.savefig(os.path.join(image_dir, 'graph_image_{}_{}'.format(snapshot, projection)))
+
+
+def plot_from_big_ds_item(ds_item):
+    print('extracting data...')
+    t0 = default_timer()
+    (graph, image, snapshot, projection) = ds_item
+    print('done!', default_timer() - t0)
+    print('to nx_graph...')
+    t0 = default_timer()
+    nx_graph = nx.DiGraph()
+
+    pos = dict()  # for plotting node positions.
+    dens_list_i = []
+    mass_list = []
+    vol_list = []
+    x_list_i = []
+    y_list_i = []
+    z_list_i = []
+
+    for i, n in enumerate(graph.nodes.numpy()):
+        dens_list_i.append(n[7])
+        mass_list.append(n[9])
+        vol_list.append(n[-1])
+        x_list_i.append(n[0])
+        y_list_i.append(n[1])
+        z_list_i.append(n[2])
+
+    dens_list_i = np.array(dens_list_i)
+    top_masses = np.array(dens_list_i) > np.percentile(dens_list_i, 98)
+    print(top_masses)
+    x_list_i = np.array(x_list_i)
+    y_list_i = np.array(y_list_i)
+    z_list_i = np.array(z_list_i)
+    dens_list = dens_list_i[top_masses]
+    x_list = x_list_i[top_masses]
+    y_list = y_list_i[top_masses]
+    z_list = z_list_i[top_masses]
+
+    for i, (dens, x, y, z) in enumerate(zip(dens_list, x_list, y_list, z_list)):
+        nx_graph.add_node(i, features=[x,y,z,dens])
+        pos[i] = [x, y]
+
+    # edgelist = []
+    # for u, v in zip(graph.senders.numpy(), graph.receivers.numpy()):
+    #     nx_graph.add_edge(u, v, features=np.array([1., 0.]))
+    #     nx_graph.add_edge(v, u, features=np.array([1., 0.]))
+    #     edgelist.append((u, v))
+    #     edgelist.append((v, u))
+
+    nx_graph.graph["features"] = np.array([0.])
+    # plotting
+
+    print('done!', default_timer() - t0)
+    print('len(pos) = {}'.format(len(pos)))
+
+    print('plotting...')
+    t0 = default_timer()
+
+    # fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(60, 20))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(40, 20))
+    fig.suptitle(f'snapshot: {snapshot}, projection: {projection}', size=40)
+    ax1.set_aspect(1)
+    ax2.set_aspect(1)
+
+    draw_networkx_nodes(nx_graph, ax=ax1, pos=pos, node_size=10,
+         node_color=dens_list, cmap='viridis')
+
+    # draw_networkx_edges(nx_graph, ax=ax1, pos=pos,
+    #      edge_color='grey', width=0.1, arrowstyle='-', alpha=0.2)
+
+    ax2.imshow(image.numpy()[:, :, 0].T, origin='lower')
+
+    # weights = 10**dens_list * 10**(np.array(vol_list)[top_masses]/3.)
+    #
+    # H, xedges, yedges = np.histogram2d(x_list, y_list, bins=200, weights=weights) #, range=[[-0.5, 4], [-2,2]])
+    # print(H)
+    # H = np.where(H == 0.0, np.nan, H)
+    # ax3.imshow(np.log10(H.T), origin='lower', interpolation='nearest')    #, extent=(xmin, xmax, ymin, ymax))
+    # ax3.axis('off')
+
+    # mp_lock.acquire()  # make sure no duplicate files are made / replaced
+    # graph_image_idx = len(glob.glob(os.path.join(image_dir, 'graph_image_*')))
+    # mp_lock.release()  # make sure no duplicate files are made / replaced
+
+    print('done!', default_timer() - t0)
+    print('saving...')
+    t0 = default_timer()
+    image_dir = '/data2/hendrix/ClaudeData/M4r5b/images/'
+    plt.savefig(os.path.join(image_dir, 'graph_image_{}_{}'.format(snapshot, projection)))
+    print('done!', default_timer() - t0)
 
 
 if __name__ == '__main__':
-    examples_dir = '/data2/hendrix/examples/'
-
-    example_dirs = glob.glob(os.path.join(examples_dir, 'example_*'))
-    test_train_dir = '/data2/hendrix/train_data_2'
+    # test_train_dir = '/data2/hendrix/ClaudeData/M4r5b/'
+    # image_dir = '/data2/hendrix/ClaudeData/M4r5b/images/'
+    #
+    test_train_dir = '~/data/train_data/ClaudeData/M4r5b/'
 
     tfrecords = glob.glob(os.path.join(test_train_dir, '*.tfrecords'))  # list containing tfrecord files
     print(tfrecords)
 
+    print('making dataset...')
     # Extract the dataset (graph tuple, image, example_idx) from the tfrecords files
     dataset = tf.data.TFRecordDataset(tfrecords).map(partial(decode_examples,
                                                              node_shape=(11,),
                                                              edge_shape=(2,),
                                                              image_shape=(256, 256, 1)))  # (graph, image, idx)
+    for ds_item in iter(dataset):
+        print(ds_item)
+        plot_from_big_ds_item(ds_item)
 
-    pool = Pool(1)
-    pool.map(plot_from_ds_item, iter(dataset))
+    # print('mapping...')
+    # pool = Pool(1)
+    # pool.map(plot_from_big_ds_item, iter(dataset))
 
-    # dataset = dataset.apply(tf.data.experimental.ignore_errors())  # ignore corrput files
-
-    # for (graph, image, e_idx) in iter(dataset):
-    #     print(e_idx)
-    #     nx_graph = nx.DiGraph()
-    #
-    #     pos = dict()  # for plotting node positions.
-    #     dens_list = []
-    #     x_list = []
-    #     y_list = []
-    #     z_list = []
-    #     for i, n in enumerate(graph.nodes.numpy()):
-    #         nx_graph.add_node(i, features=n)
-    #         pos[i] = n[:2]
-    #         dens_list.append(n[7])
-    #         x_list.append(n[0])
-    #         y_list.append(n[1])
-    #         z_list.append(n[2])
-    #
-    #     edgelist = []
-    #     for u, v in zip(graph.senders.numpy(), graph.receivers.numpy()):
-    #         nx_graph.add_edge(u, v, features=np.array([1., 0.]))
-    #         nx_graph.add_edge(v, u, features=np.array([1., 0.]))
-    #         edgelist.append((u, v))
-    #         edgelist.append((v, u))
-    #
-    #     nx_graph.graph["features"] = np.array([0.])
-    #     # plotting
-    #
-    #     print('len(pos) = {}\nlen(edgelist) = {}'.format(len(pos), len(edgelist)))
-    #
-    #     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(60, 20))
-    #     ax1.set_aspect(1)
-    #     ax2.set_aspect(1)
-    #     ax3.set_aspect(1)
-    #
-    #     draw(nx_graph, ax=ax1, pos=pos,
-    #          edge_color='red', node_size=10, width=0.1, arrowstyle='-',
-    #          node_color=dens_list, cmap='viridis')
-    #
-    #     ax2.imshow(image.numpy()[:, :, 0])
-    #     ax2.axis('off')
-    #
-    #     _x = np.linspace(np.min(x_list), np.max(x_list), 265)
-    #     _y = np.linspace(np.min(y_list), np.max(y_list), 265)
-    #     _z = np.linspace(np.min(z_list), np.max(z_list), 265)
-    #     x, y, z = np.meshgrid(_x, _y, _z, indexing='ij')
-    #
-    #     interp = interpolate.griddata((x_list, y_list, z_list),
-    #                                   dens_list,
-    #                                   xi=(x, y, z), fill_value=np.min(dens_list))
-    #
-    #     im = np.sum(interp, axis=2)
-    #     ax3.imshow(im)
-    #     ax3.axis('off')
-    #
-    #     image_dir = '/data2/hendrix/images_tf/'
-    #     graph_image_idx = len(glob.glob(os.path.join(image_dir, 'graph_image_*')))
-    #     plt.savefig(os.path.join(image_dir, 'graph_image_{}'.format(graph_image_idx)))
-    #
-    # # get_data_info(example_dirs)
-    # # get_data_image(example_dirs)
