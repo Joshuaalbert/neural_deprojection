941c48f0257e92548a9d5e9eac9930a86b273988
joshuaalbert <albert@strw.leidenuniv.nl>
2021-03-16 13:59:19 -0400

    give better name to grads histograms

--- neural_deprojection/graph_net_utils.py
+++ neural_deprojection/graph_net_utils.py
@@ -80,7 +80,7 @@ class TrainOneEpoch(Module):
             replica_ctx = tf.distribute.get_replica_context()
             grads = replica_ctx.all_reduce("mean", grads)
         for (param, grad) in zip(params, grads):
-            tf.summary.histogram(param.name,grad, step=self.minibatch)
+            tf.summary.histogram(param.name+"_grad",grad, step=self.minibatch)
         self.opt.apply(grads, params)
         return loss
 
