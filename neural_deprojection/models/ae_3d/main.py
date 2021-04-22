3404ee465bd880835746e819e78866f275bfe6e4
Matthijs van Groeningen <matthijsvangroeningen@gmail.com>
2021-04-22 18:56:21 +0200

    runs, but oom error if batch > 2

--- neural_deprojection/models/ae_3d/main.py
+++ neural_deprojection/models/ae_3d/main.py
@@ -158,7 +158,7 @@ class RelationNetwork(AbstractModule):
         output_graph = self._global_block(edge_block)
         return output_graph
 
-# TODO: give option to feed position in the core network
+
 class EncodeProcessDecode(AbstractModule):
     """Full encode-process-decode model.
     The model we explore includes three components:
@@ -205,8 +205,7 @@ class EncodeProcessDecode(AbstractModule):
 
 class CoreNetwork(AbstractModule):
     """
-    Core network which can be used in the EncodeProcessDecode network. Consists of a (full) graph network block
-    and a self attention block.
+    Core network which can be used in the EncodeProcessDecode network. Closely follows the transformer architecture.
     """
 
     def __init__(self,
@@ -280,97 +279,35 @@ class EncoderNetwork(AbstractModule):
 
 class DecoderNetwork(AbstractModule):
     """
-    Encoder network that updates the graph to viable input for the Core network.
-    Contains a node block to update the edges and a relation network to generate edges and globals.
+    Decoder network that updates the nodes of a graph.
+    By default only uses the globals to update the nodes, but can also use nodes and edges.
     """
 
     def __init__(self,
                  node_model_fn,
+                 use_received_edges=False,
+                 use_sent_edges=False,
+                 use_nodes=False,
+                 use_globals=True,
                  name=None):
         super(DecoderNetwork, self).__init__(name=name)
         self.node_block = blocks.NodeBlock(node_model_fn,
-                                           use_received_edges=False,
-                                           use_sent_edges=False,
-                                           use_nodes=False,
-                                           use_globals=True)
+                                           use_received_edges=use_received_edges,
+                                           use_sent_edges=use_sent_edges,
+                                           use_nodes=use_nodes,
+                                           use_globals=use_globals)
 
-    def _build(self, input_graph):
+    def _build(self, input_graph, positions):
         output = self.node_block(input_graph)
+
+        if positions is not None:
+            prepend_nodes = tf.concat([positions, output.nodes[:, 3:]], axis=1)
+            output.replace(nodes=prepend_nodes)
+
         return output
 
 
-class AutoEncoder(AbstractModule):
-    def __init__(self, kernel_size=4, name=None):
-        super(AutoEncoder, self).__init__(name=name)
-        self.encoder = snt.Sequential([snt.Conv2D(4, kernel_size, stride=4, padding='SAME'), tf.nn.relu,
-                                       snt.Conv2D(16, kernel_size, stride=4, padding='SAME'), tf.nn.relu,
-                                       snt.Conv2D(64, kernel_size, stride=4, padding='SAME'), tf.nn.relu])
-
-        self.decoder = snt.Sequential([snt.Conv2DTranspose(64, kernel_size, stride=4, padding='SAME'), tf.nn.relu,
-                                       snt.Conv2DTranspose(16, kernel_size, stride=4, padding='SAME'), tf.nn.relu,
-                                       snt.Conv2DTranspose(4, kernel_size, stride=4, padding='SAME'), tf.nn.relu,
-                                       snt.Conv2D(1, kernel_size, padding='SAME')])
-
-    @property
-    def step(self):
-        if self._step is None:
-            raise ValueError("Need to set step idx variable. model.step = epoch")
-        return self._step
-
-    @step.setter
-    def step(self, value):
-        self._step = value
-
-    def _build(self, batch):
-        (img, ) = batch
-        img = gaussian_filter2d(img, filter_shape=[6, 6])
-        img_before_autoencoder = (img - tf.reduce_min(img)) / (
-                tf.reduce_max(img) - tf.reduce_min(img))
-        tf.summary.image(f'img_before_autoencoder', img_before_autoencoder, step=self.step)
-        encoded_img = self.encoder(img)
-
-        print(encoded_img.shape)
-
-        decoded_img = self.decoder(encoded_img)
-        img_after_autoencoder = (decoded_img - tf.reduce_min(decoded_img)) / (
-                tf.reduce_max(decoded_img) - tf.reduce_min(decoded_img))
-        tf.summary.image(f'img_after_autoencoder', img_after_autoencoder, step=self.step)
-        return decoded_img
-
-
-class Model(AbstractModule):
-    """Model inherits from AbstractModule, which contains a __call__ function which executes a _build function
-    that is to be specified in the child class. So for example:
-    model = Model(), then model() returns the output of _build()
-
-    AbstractModule inherits from snt.Module, which has useful functions that can return the (trainable) variables,
-    so the Model class has this functionality as well
-    An instance of the RelationNetwork class also inherits from AbstractModule,
-    so it also executes its _build() function when called and it can return its (trainable) variables
-
-
-    A RelationNetwork contains an edge block and a global block:
-
-    The edge block generally uses the edge, receiver, sender and global attributes of the input graph
-    to calculate the new edges.
-    In our case we currently only use the receiver and sender attributes to calculate the edges.
-
-    The global block generally uses the aggregated edge, aggregated node and the global attributes of the input graph
-    to calculate the new globals.
-    In our case we currently only use the aggregated edge attributes to calculate the new globals.
-
-
-    As input the RelationNetwork needs two (neural network) functions:
-    one to calculate the new edges from receiver and sender nodes
-    and one to calculate the globals from the aggregated edges.
-
-    The new edges will be a vector with size 16 (i.e. the output of the first function in the RelationNetwork)
-    The new globals will also be a vector with size 16 (i.e. the output of the second function in the RelationNetwork)
-
-    The image_cnn downscales the image (currently from 4880x4880 to 35x35) and encodes the image in 16 channels.
-    So we (currently) go from (4880,4880,1) to (35,35,16)
-    """
-
+class GraphAutoEncoder(AbstractModule):
     def __init__(self,
                  mlp_size=16,
                  cluster_encoded_size=10,
@@ -379,7 +316,7 @@ class Model(AbstractModule):
                  kernel_size=4,
                  image_feature_size=16,
                  core_steps=10, name=None):
-        super(Model, self).__init__(name=name)
+        super(GraphAutoEncoder, self).__init__(name=name)
 
         self.epd_encoder = EncodeProcessDecode(encoder=EncoderNetwork(edge_model_fn=lambda: snt.nets.MLP([mlp_size], activate_final=True),
                                                                       node_model_fn=lambda: snt.Linear(cluster_encoded_size),
@@ -391,15 +328,12 @@ class Model(AbstractModule):
                                                                       node_model_fn=lambda: snt.Linear(cluster_encoded_size),
                                                                       global_model_fn=lambda: snt.nets.MLP([32, 32, 64], activate_final=True)))
 
-        self.epd_decoder = EncodeProcessDecode(encoder=EncoderNetwork(edge_model_fn=lambda: snt.nets.MLP([mlp_size], activate_final=True),
-                                                                      node_model_fn=lambda: snt.Linear(cluster_encoded_size),
-                                                                      global_model_fn=lambda: snt.nets.MLP([mlp_size], activate_final=True)),
+        self.epd_decoder = EncodeProcessDecode(encoder=DecoderNetwork(node_model_fn=lambda: snt.Linear(cluster_encoded_size)),
                                                core=CoreNetwork(num_heads=num_heads,
                                                                 multi_head_output_size=cluster_encoded_size,
                                                                 input_node_size=cluster_encoded_size),
-                                               decoder=EncoderNetwork(edge_model_fn=lambda: snt.nets.MLP([mlp_size], activate_final=True),
-                                                                      node_model_fn=lambda: snt.Linear(cluster_encoded_size),
-                                                                      global_model_fn=lambda: snt.nets.MLP([32, 32, 64], activate_final=True)))
+                                               decoder=DecoderNetwork(node_model_fn=lambda: snt.Linear(cluster_encoded_size),
+                                                                      use_nodes=True))
 
         self.image_feature_size = image_feature_size
 
@@ -416,9 +350,7 @@ class Model(AbstractModule):
         self._step = value
 
     def _build(self, batch, *args, **kwargs):
-        (graph, img, c) = batch
-        del img
-        del c
+        graph = batch
 
         positions = graph.nodes[:, 0:3]
 
@@ -426,38 +358,6 @@ class Model(AbstractModule):
         decoded_graph = self.epd_decoder(encoded_graph, self._core_steps, positions)
 
         return decoded_graph
-
-MODEL_MAP = dict(model1=Model)
-
-
-def build_training(model_type, model_parameters, optimizer_parameters, loss_parameters, strategy=None) -> TrainOneEpoch:
-    model_cls = MODEL_MAP[model_type]
-
-    model = model_cls(**model_parameters)
-
-    def build_opt(**kwargs):
-        opt_type = kwargs.get('opt_type')
-        if opt_type == 'adam':
-            learning_rate = kwargs.get('learning_rate', 1e-4)
-            opt = snt.optimizers.Adam(learning_rate)
-        else:
-            raise ValueError('Opt {} invalid'.format(opt_type))
-        return opt
-
-
-    def build_loss(**loss_parameters):
-        def loss(model_outputs, batch):
-            (graph, img, c) = batch
-            # loss =  mean(-sum_k^2 true[k] * log(pred[k]/true[k]))
-            return tf.reduce_mean(tf.losses.binary_crossentropy(c[None, None], model_outputs, from_logits=True))# tf.math.sqrt(tf.reduce_mean(tf.math.square(rank - tf.nn.sigmoid(model_outputs[:, 0]))))
-        return loss
-
-    loss = build_loss(**loss_parameters)
-    opt = build_opt(**optimizer_parameters)
-
-    training = TrainOneEpoch(model, loss, opt, strategy=strategy)
-
-    return training
 
 
 def feature_to_graph_tuple(name=''):
@@ -467,7 +367,10 @@ def feature_to_graph_tuple(name=''):
             f'{name}_receivers': tf.io.FixedLenFeature([], dtype=tf.string)}
 
 
-def decode_examples(record_bytes, node_shape=None, edge_shape=None, image_shape=None):
+def decode_examples(record_bytes,
+                    node_shape=None,
+                    edge_shape=None,
+                    image_shape=None):
     """
     Decodes raw bytes as returned from tf.data.TFRecordDataset([example_path]) into a GraphTuple and image
     Args:
@@ -507,10 +410,12 @@ def decode_examples(record_bytes, node_shape=None, edge_shape=None, image_shape=
     graph_edges = tf.io.parse_tensor(parsed_example['graph_edges'], tf.float32)
     if edge_shape is not None:
         graph_edges.set_shape([None] + list(edge_shape))
-    receivers = tf.io.parse_tensor(parsed_example['graph_receivers'], tf.int32)
+    receivers = tf.io.parse_tensor(parsed_example['graph_receivers'], tf.int64)
     receivers.set_shape([None])
-    senders = tf.io.parse_tensor(parsed_example['graph_senders'], tf.int32)
+    receivers = tf.cast(receivers, tf.int32)
+    senders = tf.io.parse_tensor(parsed_example['graph_senders'], tf.int64)
     senders.set_shape([None])
+    senders = tf.cast(senders, tf.int32)
     graph = GraphsTuple(nodes=graph_nodes,
                         edges=graph_edges,
                         globals=tf.zeros([1]),
@@ -561,9 +466,6 @@ def build_dataset(tfrecords):
 
 
 def train_ae_3d(data_dir, config):
-    # strategy = get_distribution_strategy(use_cpus=False, logical_per_physical_factor=1, memory_limit=10000)
-
-    # lists containing tfrecord files
     train_tfrecords = glob.glob(os.path.join(data_dir, 'train', '*.tfrecords'))
     test_tfrecords = glob.glob(os.path.join(data_dir, 'test', '*.tfrecords'))
 
@@ -574,39 +476,28 @@ def train_ae_3d(data_dir, config):
     train_dataset = build_dataset(train_tfrecords)
     test_dataset = build_dataset(test_tfrecords)
 
-    # train_dataset = train_dataset.map(lambda graph, img, c: (graph,))
-    # test_dataset = test_dataset.map(lambda graph, img, c: (graph,))
+    train_dataset = train_dataset.map(lambda graph, img, c: (graph,))
+    test_dataset = test_dataset.map(lambda graph, img, c: (graph,))
 
-    # for ds_item in iter(train_dataset):
-    #     print(ds_item)
-    #     break
+    train_dataset = batch_dataset_set_graph_tuples(all_graphs_same_size=True, dataset=train_dataset, batch_size=2)
+    test_dataset = batch_dataset_set_graph_tuples(all_graphs_same_size=True, dataset=test_dataset, batch_size=2)
 
-    train_dataset = batch_dataset_set_graph_tuples(all_graphs_same_size=True, dataset=train_dataset, batch_size=32)
-    test_dataset = batch_dataset_set_graph_tuples(all_graphs_same_size=True, dataset=test_dataset, batch_size=32)
-
-    # for ds_item in iter(train_dataset):
-    #     print(ds_item)
-    #     break
-
-    model = Model(mlp_size=16,
-                  cluster_encoded_size=10,
-                  image_encoded_size=64,
-                  num_heads=10,
-                  kernel_size=4,
-                  image_feature_size=16,
-                  core_steps=10, name=None)
+    model = GraphAutoEncoder(mlp_size=16,
+                             cluster_encoded_size=10,
+                             image_encoded_size=64,
+                             num_heads=10,
+                             kernel_size=4,
+                             image_feature_size=16,
+                             core_steps=10, name=None)
 
     learning_rate = 1e-5
     opt = snt.optimizers.Adam(learning_rate)
 
     def loss(model_outputs, batch):
-        (graph, img, c) = batch
-        del img
-        del c
+        graph = batch
         decoded_graph = model_outputs
         return tf.reduce_mean((graph.nodes - decoded_graph.nodes) ** 2)
 
-    # with strategy.scope():
     train_one_epoch = TrainOneEpoch(model, loss, opt, strategy=None)
 
     log_dir = build_log_dir('test_log_dir', config)
@@ -616,48 +507,6 @@ def train_ae_3d(data_dir, config):
                           training_dataset=train_dataset,
                           test_dataset=test_dataset,
                           num_epochs=20,
-                          early_stop_patience=5,
-                          checkpoint_dir=checkpoint_dir,
-                          log_dir=log_dir,
-                          debug=False)
-
-def train_autoencoder(data_dir):
-    # strategy = get_distribution_strategy(use_cpus=False, logical_per_physical_factor=1, memory_limit=10000)
-
-    # lists containing tfrecord files
-    train_tfrecords = glob.glob(os.path.join(data_dir, 'train', '*.tfrecords'))
-    test_tfrecords = glob.glob(os.path.join(data_dir, 'test', '*.tfrecords'))
-
-    print(f'Number of training tfrecord files : {len(train_tfrecords)}')
-    print(f'Number of test tfrecord files : {len(test_tfrecords)}')
-    print(f'Total : {len(train_tfrecords) + len(test_tfrecords)}')
-
-    train_dataset = build_dataset(train_tfrecords)
-    test_dataset = build_dataset(test_tfrecords)
-
-    train_dataset = train_dataset.map(lambda graph, img, c: (img,)).batch(batch_size=32)
-    test_dataset = test_dataset.map(lambda graph, img, c: (img,)).batch(batch_size=32)
-
-    # with strategy.scope():
-    model = AutoEncoder(kernel_size=4)
-
-    learning_rate = 1e-5
-    opt = snt.optimizers.Adam(learning_rate)
-
-    def loss(model_outputs, batch):
-        (img,) = batch
-        decoded_img = model_outputs
-        return tf.reduce_mean((gaussian_filter2d(img, filter_shape=[6, 6]) - decoded_img[:, 12:-12, 12:-12, :]) ** 2)
-
-    train_one_epoch = TrainOneEpoch(model, loss, opt, strategy=None)
-
-    log_dir = 'autoencoder_log_dir'
-    checkpoint_dir = 'autoencoder_checkpointing'
-
-    vanilla_training_loop(train_one_epoch=train_one_epoch,
-                          training_dataset=train_dataset,
-                          test_dataset=test_dataset,
-                          num_epochs=50,
                           early_stop_patience=5,
                           checkpoint_dir=checkpoint_dir,
                           log_dir=log_dir,
@@ -673,23 +522,16 @@ if __name__ == '__main__':
     tfrec_dir = os.path.join(tfrec_base_dir, 'snap_128_tf_records')
 
     learning_rate = 1e-5
-    kernel_size = 4
-    # mlp_layers = 2
-    image_feature_size = 64  # 64
-    # conv_layers = 6
-    # mlp_layer_nodes = 32
-    mlp_size = 16  # 16
-    cluster_encoded_size = 10  # 10
-    image_encoded_size = 64  # 64
+    mlp_size = 16
+    cluster_encoded_size = 10
+    image_encoded_size = 64
     core_steps = 28
-    num_heads = 4
+    num_heads = 1
 
     config = dict(model_type='model1',
                   model_parameters=dict(mlp_size=mlp_size,
                                         cluster_encoded_size=cluster_encoded_size,
                                         image_encoded_size=image_encoded_size,
-                                        kernel_size=kernel_size,
-                                        image_feature_size=image_feature_size,
                                         core_steps=core_steps,
                                         num_heads=num_heads),
                   optimizer_parameters=dict(learning_rate=learning_rate, opt_type='adam'),
