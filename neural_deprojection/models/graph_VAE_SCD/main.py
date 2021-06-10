48f567c7bb465957330bd3a8ecf6174d28651a26
JuliusHendrix <jlam.hendrix@gmail.com>
2021-06-10 19:12:04 +0200

    * SCD specific dVAE

--- neural_deprojection/models/graph_VAE_SCD/main.py
+++ neural_deprojection/models/graph_VAE_SCD/main.py
@@ -1,13 +1,15 @@
 import sys
+
 sys.path.insert(1, '/data/s1825216/git/neural_deprojection/')
 
 from functools import partial
 
 from graph_nets.graphs import GraphsTuple
 
-from neural_deprojection.models.identify_medium_SCD.generate_data import decode_examples
+from neural_deprojection.models.identify_medium_SCD.generate_data import decode_examples, decode_examples_old
 
-from neural_deprojection.models.graph_VAE_SCD.graph_VAE_utils import Model
+from neural_deprojection.models.graph_VAE_SCD.graph_VAE_utils import Model, DiscreteGraphVAE, EncoderNetwork3D, \
+    DecoderNetwork3D
 from neural_deprojection.graph_net_utils import vanilla_training_loop, TrainOneEpoch, AbstractModule, \
     get_distribution_strategy, build_log_dir, build_checkpoint_dir, batch_dataset_set_graph_tuples
 import glob, os
@@ -16,9 +18,12 @@ import json
 import sonnet as snt
 import numpy as np
 
-MODEL_MAP = {'model1': Model}
+MODEL_MAP = {'model1': Model,
+             'discvae': DiscreteGraphVAE}
 
-def build_training(model_type, model_parameters, optimizer_parameters, loss_parameters, strategy=None, **kwargs) -> TrainOneEpoch:
+
+def build_training(model_type, model_parameters, optimizer_parameters, loss_parameters, strategy=None,
+                   **kwargs) -> TrainOneEpoch:
     model_cls = MODEL_MAP[model_type]
     model = model_cls(**model_parameters, **kwargs)
 
@@ -32,10 +37,20 @@ def build_training(model_type, model_parameters, optimizer_parameters, loss_para
         return opt
 
     def build_loss(**loss_parameters):
-        def loss(model_outputs, batch):
-            graph = batch
-            decoded_graph, nn_index = model_outputs
-            return tf.reduce_mean((tf.gather(graph.nodes[:, 3:], nn_index) - decoded_graph.nodes) ** 2 * tf.constant([0,0,0,0,1,0,0,0], dtype=graph.nodes.dtype))
+        if model_type == 'model2':
+            def loss(model_outputs, batch):
+                graph = batch
+                decoded_graph, nn_index = model_outputs
+                return tf.reduce_mean((tf.gather(graph.nodes[:, 3:], nn_index) - decoded_graph.nodes) ** 2 * tf.constant(
+                    [0, 0, 0, 0, 1, 0, 0, 0], dtype=graph.nodes.dtype))
+        else:
+            def loss(model_outputs, batch):
+                graph = batch
+                # model_outputs = dict(loss=tf.reduce_mean(log_likelihood_samples - kl_term_samples),
+                # var_exp=tf.reduce_mean(log_likelihood_samples),
+                # kl_term=tf.reduce_mean(kl_term_samples),
+                # mean_perplexity=mean_perplexity)
+                return model_outputs['loss']
         return loss
 
     loss = build_loss(**loss_parameters)
@@ -56,7 +71,7 @@ def build_dataset(data_dir):
     Returns: Dataset obj.
     """
     tfrecords = glob.glob(os.path.join(data_dir, '*.tfrecords'))
-    dataset = tf.data.TFRecordDataset(tfrecords).map(partial(decode_examples,
+    dataset = tf.data.TFRecordDataset(tfrecords).map(partial(decode_examples_old,
                                                              node_shape=(11,),
                                                              image_shape=(256, 256, 1)))  # (graph, image, spsh, proj)
 
@@ -112,21 +127,92 @@ def train_ae_3d(data_dir, config):
                           early_stop_patience=10,
                           checkpoint_dir=checkpoint_dir,
                           log_dir=log_dir,
-                          debug=True)
+                          debug=False)
+
+
+def train_disc_graph_vae(data_dir, config):
+    # strategy = get_distribution_strategy(use_cpus=True, logical_per_physical_factor=1)
+
+    # lists containing tfrecord files
+    train_dataset = build_dataset(os.path.join(data_dir, 'train'))
+    test_dataset = build_dataset(os.path.join(data_dir, 'test'))
+
+    # train_dataset = train_dataset.map(lambda graph, img, c: (graph,))
+    # test_dataset = test_dataset.map(lambda graph, img, c: (graph,))
+
+    # for ds_item in iter(train_dataset):
+    #     print(ds_item)
+    #     break
+
+    # for ds_item in iter(train_dataset):
+    #     print(ds_item)
+    #     br
+
+    train_one_epoch = build_training(model_parameters=dict(encoder_fn=EncoderNetwork3D,
+                                                           decode_fn=DecoderNetwork3D,
+                                                           embedding_dim=4,  # 64
+                                                           num_embedding=4,  # 64
+                                                           num_gaussian_components=4,  # 128
+                                                           num_token_samples=1,
+                                                           num_properties=8,
+                                                           temperature=50.,
+                                                           beta=1.,
+                                                           encoder_kwargs=dict(inter_graph_connect_prob=0.01,
+                                                                               reducer=tf.math.unsorted_segment_mean,
+                                                                               starting_global_size=4,
+                                                                               node_size=4,  # 64
+                                                                               edge_size=4,
+                                                                               crossing_steps=1,
+                                                                               name=None),
+                                                           decode_kwargs=dict(inter_graph_connect_prob=0.01,
+                                                                              reducer=tf.math.unsorted_segment_mean,
+                                                                              starting_global_size=4,
+                                                                              node_size=4,  # 64
+                                                                              edge_size=4,
+                                                                              crossing_steps=1,
+                                                                              name=None),
+                                                           name=None),
+                                     **config)
+
+    # log_dir = build_log_dir('test_log_dir', config)
+    # checkpoint_dir = build_checkpoint_dir('test_checkpointing', config)
+    log_dir = 'dVAE_log_dir'
+    checkpoint_dir = 'dVAE_checkpointing'
+    model_dir = 'dVAE_model'
+
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    with open(os.path.join(checkpoint_dir, 'config.json'), 'w') as f:
+        json.dump(config, f)
+
+    vanilla_training_loop(train_one_epoch=train_one_epoch,
+                          training_dataset=train_dataset,
+                          test_dataset=test_dataset,
+                          num_epochs=100,
+                          early_stop_patience=10,
+                          checkpoint_dir=checkpoint_dir,
+                          log_dir=log_dir,
+                          save_model_dir=model_dir,
+                          debug=False)
 
 
 def main(data_dir):
-    config = dict(model_type='model1',
-                  model_parameters=dict(activation='leaky_relu',
-                      mlp_size=16,
-                      cluster_encoded_size=11,
-                      num_heads=1,
-                      core_steps=15,
-                      name='with_random_positions'),
+    # config = dict(model_type='model1',
+    #               model_parameters=dict(activation='leaky_relu',
+    #                   mlp_size=16,
+    #                   cluster_encoded_size=11,
+    #                   num_heads=1,
+    #                   core_steps=15,
+    #                   name='with_random_positions'),
+    #               optimizer_parameters=dict(learning_rate=1e-5,
+    #                                         opt_type='adam'),
+    #               loss_parameters=dict())
+
+    config = dict(model_type='discvae',
                   optimizer_parameters=dict(learning_rate=1e-5,
                                             opt_type='adam'),
                   loss_parameters=dict())
-    train_ae_3d(data_dir, config)
+
+    train_disc_graph_vae(data_dir, config)
 
 
 if __name__ == '__main__':
