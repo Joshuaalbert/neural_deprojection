e8a87f00264deb80aa3273ecc41a8b9297068d8f
JuliusHendrix <jlam.hendrix@gmail.com>
2021-06-15 10:01:14 +0200

    * work session 14-06

---
+++ neural_deprojection/models/Simple_complete_model/graph_decoder.py
@@ -0,0 +1,228 @@
+from graph_nets import blocks
+from graph_nets.utils_tf import concat
+
+import tensorflow as tf
+import sonnet as snt
+from graph_nets.graphs import GraphsTuple
+from graph_nets import utils_tf
+from neural_deprojection.graph_net_utils import AbstractModule, gaussian_loss_function, \
+    get_shape, graph_batch_reshape, graph_unbatch_reshape, replicate_graph, sort_graph
+import tensorflow_probability as tfp
+
+
+def _create_autogressive_edges_from_nodes_dynamic(n_node, exclude_self_edges):
+    """Creates complete edges for a graph with `n_node`.
+
+    Args:
+    n_node: (integer scalar `Tensor`) The number of nodes.
+    exclude_self_edges: (bool) Excludes self-connected edges.
+
+    Returns:
+    A dict of RECEIVERS, SENDERS and N_EDGE data (`Tensor`s of rank 1).
+    """
+
+
+    rng = tf.range(n_node)
+
+    if exclude_self_edges:
+        ind = rng[:, None] < rng
+        n_edge = n_node * (n_node - 1) // 2
+    else:
+        ind = rng[:, None] <= rng
+        n_edge = n_node * (n_node - 1) // 2 + n_node
+
+    receivers, senders = tf.where(ind)
+
+
+    receivers = tf.reshape(tf.cast(receivers, tf.int32), [n_edge])
+    senders = tf.reshape(tf.cast(senders, tf.int32), [n_edge])
+    n_edge = tf.reshape(n_edge, [1])
+
+    return {'receivers': receivers, 'senders': senders, 'n_edge': n_edge}
+
+
+def autoregressive_connect_graph_dynamic(graph,
+                                exclude_self_edges=False,
+                                name="autoregressive_connect_graph_dynamic"):
+    """Adds edges to a graph by fully-connecting the nodes.
+
+    This method does not require the number of nodes per graph to be constant,
+    or to be known at graph building time.
+
+    Args:
+    graph: A `graphs.GraphsTuple` with `None` values for the edges, senders and
+      receivers.
+    exclude_self_edges (default=False): Excludes self-connected edges.
+    name: (string, optional) A name for the operation.
+
+    Returns:
+    A `graphs.GraphsTuple` containing `Tensor`s with fully-connected edges.
+
+    Raises:
+    ValueError: if any of the `EDGES`, `RECEIVERS` or `SENDERS` field is not
+      `None` in `graph`.
+    """
+    utils_tf._validate_edge_fields_are_all_none(graph)
+
+    with tf.name_scope(name):
+
+        def body(i, senders, receivers, n_edge):
+            edges = _create_autogressive_edges_from_nodes_dynamic(graph.n_node[i],
+                                                            exclude_self_edges)
+            return (i + 1, senders.write(i, edges['edges']),
+                  receivers.write(i, edges['receivers']),
+                  n_edge.write(i, edges['n_edge']))
+
+        num_graphs = utils_tf.get_num_graphs(graph)
+        loop_condition = lambda i, *_: tf.less(i, num_graphs)
+        initial_loop_vars = [0] + [
+            tf.TensorArray(dtype=tf.int32, size=num_graphs, infer_shape=False)
+                                   for _ in range(3)  # senders, receivers, n_edge
+                                  ]
+        _, senders_array, receivers_array, n_edge_array = tf.while_loop(
+            loop_condition, body, initial_loop_vars, back_prop=False)
+
+        n_edge = n_edge_array.concat()
+        offsets = utils_tf._compute_stacked_offsets(graph.n_node, n_edge)
+        senders = senders_array.concat() + offsets
+        receivers = receivers_array.concat() + offsets
+        senders.set_shape(offsets.shape)
+        receivers.set_shape(offsets.shape)
+
+        receivers.set_shape([None])
+        senders.set_shape([None])
+
+        num_graphs = graph.n_node.get_shape().as_list()[0]
+        n_edge.set_shape([num_graphs])
+
+        return graph._replace(senders=senders, receivers=receivers, n_edge=n_edge)
+
+
+class GraphMappingNetwork(AbstractModule):
+    """
+    Encoder network that updates the graph to viable input for the DiscreteGraphVAE network.
+    """
+
+    def __init__(self,
+                 num_output: int,
+                 output_size: int,
+                 node_size: int = 4,
+                 edge_size: int = 4,
+                 starting_global_size: int = 10,
+                 inter_graph_connect_prob: float = 0.01,
+                 crossing_steps: int = 4,
+                 reducer=tf.math.unsorted_segment_mean,
+                 properties_size=10,
+                 name=None):
+        super(GraphMappingNetwork, self).__init__(name=name)
+        self.num_output = num_output
+        self.output_size = output_size
+        self.crossing_steps = crossing_steps
+        self.empty_node_variable = tf.Variable(initial_value=tf.random.truncated_normal((node_size,)),
+                                               name='empty_token_node')
+
+        # values for different kinds of edges in the graph, which will be learned
+        self.intra_graph_edge_variable = tf.Variable(initial_value=tf.random.truncated_normal((edge_size,)),
+                                                     name='intra_graph_edge_var')
+        self.intra_token_graph_edge_variable = tf.Variable(initial_value=tf.random.truncated_normal((edge_size,)),
+                                                           name='intra_token_graph_edge_var')
+        self.inter_graph_edge_variable = tf.Variable(initial_value=tf.random.truncated_normal((edge_size,)),
+                                                     name='inter_graph_edge_var')
+        self.starting_global_variable = tf.Variable(initial_value=tf.random.truncated_normal((starting_global_size,)),
+                                                    name='starting_global_var')
+
+        self.inter_graph_connect_prob = inter_graph_connect_prob
+
+        self.projection_node_block = blocks.NodeBlock(lambda: snt.Linear(node_size, name='project'),
+                                                      use_received_edges=False,
+                                                      use_sent_edges=False,
+                                                      use_nodes=True,
+                                                      use_globals=False)
+
+        node_model_fn = lambda: snt.nets.MLP([node_size, node_size], activate_final=True, activation=tf.nn.leaky_relu)
+        edge_model_fn = lambda: snt.nets.MLP([edge_size, edge_size], activate_final=True, activation=tf.nn.leaky_relu)
+        global_model_fn = lambda: snt.nets.MLP([starting_global_size, starting_global_size], activate_final=True,
+                                               activation=tf.nn.leaky_relu)
+
+        self.edge_block = blocks.EdgeBlock(edge_model_fn,
+                                           use_edges=False,
+                                           use_receiver_nodes=True,
+                                           use_sender_nodes=True,
+                                           use_globals=True)
+
+        self.node_block = blocks.NodeBlock(node_model_fn,
+                                           use_received_edges=True,
+                                           use_sent_edges=True,
+                                           use_nodes=True,
+                                           use_globals=True)
+
+        self.global_block = blocks.GlobalBlock(global_model_fn,
+                                               use_edges=True,
+                                               use_nodes=True,
+                                               use_globals=True,
+                                               edges_reducer=reducer)
+
+        self.output_projection_node_block = blocks.NodeBlock(lambda: snt.Linear(self.output_size, name='project'),
+                                                             use_received_edges=False,
+                                                             use_sent_edges=False,
+                                                             use_nodes=True,
+                                                             use_globals=False)
+
+    def autoregressive_connect_graph_dynamic(self, token_graphs:GraphsTuple):
+
+        n_node_per_graph = token_graphs.n_node[0]
+        (senders, receivers) = tf.where(tf.range(n_node_per_graph)[:, None] <= tf.range(n_node_per_graph))
+
+
+
+
+    def _build(self, graphs):
+        """
+        Adds another set of nodes to each graph. Autoregressively links all nodes in a graph.
+
+        Args:
+            graphs: batched GraphsTuple
+
+        Returns:
+
+        """
+        # give graphs edges and new node dimension (linear transformation)
+        graphs = self.projection_node_block(graphs)  # [n_node, node_size]
+        batched_graphs = graph_batch_reshape(graphs) #nodes = [n_graphs, n_node_per_graph, dim]
+        [n_graphs, n_node_per_graph, _] = get_shape(batched_graphs.nodes)
+        concat_nodes = tf.concat([batched_graphs.nodes, tf.tile(self.empty_node_variable[None, :], [n_graphs, self.num_output, 1])], axis=0)
+        batched_graphs = batched_graphs.replace(nodes=concat_nodes,
+                                                globals=tf.tile(self.global_block[None,:],[n_graphs]))
+        concat_graphs = graph_unbatch_reshape(batched_graphs) #[n_node+num_output, node_size]
+
+        #nodes, senders, recievers, globals
+        concat_graphs = self.autoregressive_connect_graph_dynamic(concat_graphs)
+
+        latent_graphs = concat_graphs
+        def _core(latent_graphs):
+            input_nodes = latent_graphs.nodes
+            latent_graphs = self.edge_block(latent_graphs)
+            latent_graphs = self.node_block(latent_graphs)
+            latent_graphs = self.global_block(latent_graphs)
+            #todo: mask
+            latent_graphs = latent_graphs.replace(nodes=latent_graphs.nodes + input_nodes)  # residual connections
+            return latent_graphs
+
+        _, latent_graphs = tf.while_loop(cond=lambda const, state: const < self.num_output,
+                                        body=lambda const, state: (const + 1, _core(state)),
+                                        loop_vars=(tf.constant(0), latent_graphs))
+
+        latent_graphs = graph_batch_reshape(latent_graphs)
+        token_nodes = latent_graphs.nodes[:,-self.num_output:,:]
+
+        # latent_graphs = latent_graphs.replace(nodes=token_nodes,
+        #                                     edges=None,
+        #                                     receivers=None,
+        #                                     senders=None,
+        #                                     globals=None,
+        #                                     n_node=self.num_output*tf.ones(n_graphs, dtype=tf.int32),
+        #                                     n_edge=tf.constant(0, dtype=tf.int32))
+        # latent_graphs = graph_unbatch_reshape(latent_graphs)
+        # output_graphs = self.output_projection_node_block(latent_graphs)
+
+        return token_nodes
---
+++ neural_deprojection/models/Simple_complete_model/main.py
@@ -0,0 +1,117 @@
+import tensorflow as tf
+import sonnet as snt
+from graph_nets.graphs import GraphsTuple
+from neural_deprojection.graph_net_utils import vanilla_training_loop, TrainOneEpoch, \
+    get_distribution_strategy, build_log_dir, build_checkpoint_dir
+from neural_deprojection.models.Simple_complete_model.graph_networks import DiscreteGraphVAE
+from neural_deprojection.models.identify_medium.generate_data import decode_examples
+import glob, os, json
+
+MODEL_MAP = dict(dis_graph_vae=DiscreteGraphVAE)
+
+
+def build_training(model_type, model_parameters, optimizer_parameters, loss_parameters, strategy=None,
+                   **kwargs) -> TrainOneEpoch:
+    model_cls = MODEL_MAP[model_type]
+    model = model_cls(**model_parameters, **kwargs)
+
+    def build_opt(**kwargs):
+        opt_type = kwargs.get('opt_type')
+        if opt_type == 'adam':
+            learning_rate = kwargs.get('learning_rate', 1e-4)
+            opt = snt.optimizers.Adam(learning_rate, beta1=1 - 1 / 100, beta2=1 - 1 / 500)
+        else:
+            raise ValueError('Opt {} invalid'.format(opt_type))
+        return opt
+
+    def build_loss(**loss_parameters):
+        def loss(model_outputs, batch):
+            (graph,) = batch
+            # model_outputs = dict(loss=tf.reduce_mean(log_likelihood_samples - kl_term_samples),
+            # var_exp=tf.reduce_mean(log_likelihood_samples),
+            # kl_term=tf.reduce_mean(kl_term_samples),
+            # mean_perplexity=mean_perplexity)
+            return model_outputs['loss']
+
+        return loss
+
+    loss = build_loss(**loss_parameters)
+    opt = build_opt(**optimizer_parameters)
+
+    training = TrainOneEpoch(model, loss, opt, strategy=strategy)
+
+    return training
+
+
+def build_dataset(data_dir, batch_size):
+    tfrecords = glob.glob(os.path.join(data_dir,'*.tfrecords'))
+    dataset = tf.data.TFRecordDataset(tfrecords).map(
+        lambda record_bytes: decode_examples(record_bytes, node_shape=[4]))
+    dataset = dataset.map(
+        lambda graph_data_dict, img, c: GraphsTuple(globals=tf.zeros([1]), edges=None, **graph_data_dict))
+    dataset = dataset.map(
+        lambda graph: graph._replace(nodes=tf.concat([graph.nodes[:, :3], tf.math.log(graph.nodes[:, 3:])], axis=1)))
+    dataset = dataset.map(lambda graph: (graph, ))
+    # dataset = batch_dataset_set_graph_tuples(all_graphs_same_size=True, dataset=dataset, batch_size=batch_size)
+    return dataset
+
+
+def main(data_dir, config, kwargs):
+    # Make strategy at the start of your main before any other tf code is run.
+    strategy = get_distribution_strategy(use_cpus=True, logical_per_physical_factor=1)
+
+    train_dataset = build_dataset(os.path.join(data_dir, 'train'), batch_size=4)
+    test_dataset = build_dataset(os.path.join(data_dir, 'test'), batch_size=4)
+
+    # for (graph, positions) in iter(test_dataset):
+    #     print(graph)
+    #     break
+
+    with strategy.scope():
+        train_one_epoch = build_training(**config, **kwargs)
+
+    train_one_epoch.model.set_beta(0.)
+
+    log_dir = build_log_dir('test_log_dir', config)
+    checkpoint_dir = build_checkpoint_dir('test_checkpointing', config)
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    with open(os.path.join(checkpoint_dir, 'config.json'), 'w') as f:
+        json.dump(config, f)
+
+    save_model_dir = None#os.path.join('saved_models')
+
+    vanilla_training_loop(train_one_epoch=train_one_epoch,
+                          training_dataset=train_dataset,
+                          test_dataset=test_dataset,
+                          num_epochs=1000,
+                          early_stop_patience=10,
+                          checkpoint_dir=checkpoint_dir,
+                          log_dir=log_dir,
+                          save_model_dir=save_model_dir,
+                          debug=False)
+
+
+if __name__ == '__main__':
+    config = dict(model_type='dis_graph_vae',
+                  model_parameters=dict(embedding_dim=64,
+                                        num_embedding=1024,
+                                        num_gaussian_components=128,
+                                        num_latent_tokens=64),
+                  optimizer_parameters=dict(learning_rate=1e-5, opt_type='adam'),
+                  loss_parameters=dict())
+    kwargs = dict(
+        num_token_samples=1,
+        num_properties=1,
+        encoder_kwargs=dict(inter_graph_connect_prob=0.01,
+                            reducer=tf.math.unsorted_segment_mean,
+                            starting_global_size=4,
+                            node_size=64,
+                            edge_size=4,
+                            crossing_steps=4, ),
+        decode_kwargs=dict(inter_graph_connect_prob=0.01,
+                           reducer=tf.math.unsorted_segment_mean,
+                           starting_global_size=4,
+                           node_size=64,
+                           edge_size=4,
+                           crossing_steps=4), )
+    main('../identify_medium/data_remake', config, kwargs)
