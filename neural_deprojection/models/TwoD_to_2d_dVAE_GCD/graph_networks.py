f881b2c05c8fd602855933c1e579da1b58c5ee81
Matthijs van Groeningen <matthijsvangroeningen@gmail.com>
2021-06-16 10:23:41 +0200

    Added downsampling and summaries

--- neural_deprojection/models/TwoD_to_2d_dVAE_GCD/graph_networks.py
+++ neural_deprojection/models/TwoD_to_2d_dVAE_GCD/graph_networks.py
@@ -12,51 +12,14 @@ from graph_nets.graphs import GraphsTuple
 from graph_nets.utils_tf import fully_connect_graph_dynamic, fully_connect_graph_static
 from neural_deprojection.graph_net_utils import AbstractModule, histogramdd, get_shape
 import tensorflow_probability as tfp
-from tensorflow_addons.image import gaussian_filter2d
-
-
-class ResidualStack(AbstractModule):
-    def __init__(self,
-                 num_hiddens,
-                 num_residual_layers,
-                 num_residual_hiddens,
-                 residual_name='',
-                 name=None):
-        super(ResidualStack, self).__init__(name=name)
-        self._num_hiddens = num_hiddens
-        self._num_residual_layers = num_residual_layers
-        self._num_residual_hiddens = num_residual_hiddens
-
-        self._layers = []
-        for i in range(num_residual_layers):
-            conv3 = snt.Conv2D(
-                output_channels=num_residual_hiddens,
-                kernel_shape=(3, 3),
-                stride=(1, 1),
-                name=f"res3x3_{residual_name}_{i}")
-            conv1 = snt.Conv2D(
-                output_channels=num_hiddens,
-                kernel_shape=(1, 1),
-                stride=(1, 1),
-                name=f"res1x1_{residual_name}_{i}")
-            self._layers.append((conv3, conv1))
-
-    def _build(self, inputs):
-        h = inputs
-        for conv3, conv1 in self._layers:
-            conv3_out = conv3(tf.nn.relu(h))
-            conv1_out = conv1(tf.nn.relu(conv3_out))
-            h += conv1_out
-        return tf.nn.relu(h)  # Resnet V1 style
-
+from neural_deprojection.models.openai_dvae_modules.modules import Encoder, Decoder
 
 class DiscreteImageVAE(AbstractModule):
     def __init__(self,
+                 hidden_size: int = 64,
                  embedding_dim: int = 64,
                  num_embedding: int = 1024,
-                 kernel_size: int = 4,
-                 num_token_samples: int = 1,
-                 num_properties: int = 10,
+                 num_token_samples: int = 32,
                  num_channels=1,
                  name=None):
         super(DiscreteImageVAE, self).__init__(name=name)
@@ -65,49 +28,13 @@ class DiscreteImageVAE(AbstractModule):
         self.embeddings = tf.Variable(initial_value=tf.random.truncated_normal((num_embedding, embedding_dim)),
                                       name='embeddings')
         self.num_token_samples = num_token_samples
-        self.num_properties = num_properties
         self.num_embedding = num_embedding
         self.embedding_dim = embedding_dim
-        self.residual_enc = ResidualStack(num_hiddens=32, num_residual_layers=2, num_residual_hiddens=32)
-        self.residual_dec = ResidualStack(num_hiddens=32, num_residual_layers=2, num_residual_hiddens=32)
         self.temperature = tf.Variable(initial_value=tf.constant(1.), name='temperature', trainable=False)
         self.beta = tf.Variable(initial_value=tf.constant(6.6), name='beta', trainable=False)
 
-        self.encoder = snt.Sequential(
-            [snt.Conv2D(4, kernel_size, stride=2, padding='SAME', name='conv4'), tf.nn.leaky_relu,  # [b, 512, 512, 4]
-             snt.Conv2D(8, kernel_size, stride=2, padding='SAME', name='conv8'), tf.nn.leaky_relu,  # [b, 256, 256, 8]
-             snt.Conv2D(16, kernel_size, stride=2, padding='SAME', name='conv16'), tf.nn.leaky_relu,
-             # [b, 128, 128, 16]
-             snt.Conv2D(32, kernel_size, stride=2, padding='SAME', name='conv32'), tf.nn.leaky_relu,  # [b, 64, 64, 32]
-             self.residual_enc,
-             snt.Conv2D(64, kernel_size, stride=2, padding='SAME', name='conv64'), tf.nn.leaky_relu,  # [b, 32, 32, 64]
-             snt.Conv2D(64, kernel_shape=3, stride=1, padding='SAME', name='conv_enc_1'), tf.nn.leaky_relu,
-             snt.Conv2D(self.num_embedding, kernel_shape=6, stride=1, padding='SAME', name='conv_enc_1'), tf.nn.leaky_relu])  # [b, 32, 32, num_embedding]
-
-        # self.VQVAE = snt.nets.VectorQuantizerEMA(embedding_dim=embedding_dim,
-        #                                          num_embeddings=num_embedding,
-        #                                          commitment_cost=0.25,
-        #                                          decay=0.99,
-        #                                          name='VQ')
-
-        self.decoder = snt.Sequential(
-            [snt.Conv2D(64, kernel_shape=3, stride=1, padding='SAME', name='conv_dec_1'), tf.nn.leaky_relu,
-             # [b, 32, 32, 64]
-             snt.Conv2DTranspose(32, kernel_size, stride=2, padding='SAME', name='convt32'), tf.nn.leaky_relu,
-             # [b, 64, 64, 32]
-             self.residual_dec,
-             snt.Conv2DTranspose(16, kernel_size, stride=2, padding='SAME', name='convt16'), tf.nn.leaky_relu,
-             # [b, 128, 128, 16]
-             snt.Conv2DTranspose(8, kernel_size, stride=2, padding='SAME', name='convt8'), tf.nn.leaky_relu,
-             # [b, 256, 256, 8]
-             snt.Conv2DTranspose(4, kernel_size, stride=2, padding='SAME', name='convt4'), tf.nn.leaky_relu,
-             # [b, 512, 512, 4]
-             snt.Conv2DTranspose(1, kernel_size, stride=2, padding='SAME', name='convt1'),
-             tf.nn.leaky_relu])  # [b, 1024, 1024, 1]
-
-    # @tf.function(input_signature=tf.TensorSpec(shape=[None], dtype=tf.float32))  # what is the shape ???
-    # def sample_encoder(self, graph):
-    #     return self.encoder(graph)
+        self.encoder = Encoder(hidden_size=hidden_size, num_embeddings=num_embedding, name='EncoderImage')
+        self.decoder = Decoder(hidden_size=hidden_size, num_channels=num_channels, name='DecoderImage')
 
     def set_beta(self, beta):
         self.beta.assign(beta)
@@ -115,18 +42,33 @@ class DiscreteImageVAE(AbstractModule):
     def set_temperature(self, temperature):
         self.temperature.assign(temperature)
 
-    @tf.function(input_signature=[tf.TensorSpec([None, 3], dtype=tf.float32),
-                                  tf.TensorSpec([None, None], dtype=tf.float32),
+    @tf.function(input_signature=[tf.TensorSpec([None, None, None, None], dtype=tf.float32)])
+    def sample_encoder(self, img):
+        return self.encoder(img)
+
+    @tf.function(input_signature=[tf.TensorSpec([None, None, None, None], dtype=tf.float32),
+                                  tf.TensorSpec([], dtype=tf.float32),
                                   tf.TensorSpec([], dtype=tf.float32)])
-    def sample_decoder(self, logits, temperature):
+    def sample_decoder(self, img_logits, temperature, num_samples):
+        [batch, H, W, _] = get_shape(img_logits)
+
+        logits = tf.reshape(img_logits, [batch * H * W, self.num_embedding])  # [batch*H*W, num_embeddings]
+        reduce_logsumexp = tf.math.reduce_logsumexp(logits, axis=-1)  # [batch*H*W]
+        reduce_logsumexp = tf.tile(reduce_logsumexp[:, None], [1, self.num_embedding])  # [batch*H*W, num_embedding]
+        logits -= reduce_logsumexp  # [batch*H*W, num_embeddings]
         token_distribution = tfp.distributions.RelaxedOneHotCategorical(temperature, logits=logits)
-        token_samples_onehot = token_distribution.sample((1,),
-                                                         name='token_samples')
-        token_sample_onehot = token_samples_onehot[0]  # [n_node, num_embedding]
-        token_sample = tf.matmul(token_sample_onehot, self.embeddings)  # / self.num_embedding  # [n_node, embedding_dim]  # = z ~ q(z|x)
-        latent_img = tf.reshape(token_sample, [1, 32, 32, self.embedding_dim])
-        decoded_img = self.decoder(latent_img)  # nodes=[num_gaussian_components, component_dim]
-        return decoded_img
+        token_samples_onehot = token_distribution.sample((num_samples,),
+                                                         name='token_samples')  # [S, batch*H*W, num_embeddings]
+        def _single_decode(token_sample_onehot):
+            # [batch*H*W, num_embeddings] @ [num_embeddings, embedding_dim]
+            token_sample = tf.matmul(token_samples_onehot[0], self.embeddings)  # [batch*H*W, embedding_dim]  # = z ~ q(z|x)
+            latent_img = tf.reshape(token_sample, [batch, H, W, self.embedding_dim])  # [batch, H, W, embedding_dim]
+            decoded_img = self.decoder(latent_img)  # [batch, H', W', C*2]
+            return decoded_img
+
+        decoded_ims = tf.vectorized_map(_single_decode, token_samples_onehot)  # [S, batch, H', W', C*2]
+        decoded_im = tf.reduce_mean(decoded_ims, axis=0)  # [batch, H', W', C*2]
+        return decoded_im
 
 
     def log_likelihood(self, img, mu, logb):
@@ -160,9 +102,11 @@ class DiscreteImageVAE(AbstractModule):
         [batch, H, W, _] = get_shape(encoded_img_logits)
 
         logits = tf.reshape(encoded_img_logits, [batch*H*W, self.num_embedding])  # [batch*H*W, num_embeddings]
-        logits -= tf.math.reduce_logsumexp(logits, axis=2)  # [batch*H*W, num_embeddings]
+        reduce_logsumexp = tf.math.reduce_logsumexp(logits, axis=-1)  # [batch*H*W]
+        reduce_logsumexp = tf.tile(reduce_logsumexp[:, None], [1, self.num_embedding])  # [batch*H*W, num_embedding]
+        logits -= reduce_logsumexp  # [batch*H*W, num_embeddings]
 
-        temperature = tf.maximum(0.1, 1. - 0.1/(self.step/1000))
+        temperature = tf.maximum(0.1, tf.cast(1. - 0.1/(self.step/1000), tf.float32))
         token_distribution = tfp.distributions.RelaxedOneHotCategorical(temperature, logits=logits)
         token_samples_onehot = token_distribution.sample((self.num_token_samples,), name='token_samples')  # [S, batch*H*W, num_embeddings]
 
@@ -171,26 +115,51 @@ class DiscreteImageVAE(AbstractModule):
             token_sample = tf.matmul(token_sample_onehot, self.embeddings)  # [batch*H*W, embedding_dim]  # = z ~ q(z|x)
             latent_img = tf.reshape(token_sample, [batch, H, W, self.embedding_dim])  # [batch, H, W, embedding_dim]
             decoded_img = self.decoder(latent_img)  # [batch, H', W', C*2]
+            # print('decod shape', decoded_img)
             img_mu = decoded_img[..., :self.num_channels] #[batch, H', W', C]
+            # print('mu shape', img_mu)
             img_logb = decoded_img[..., self.num_channels:]
+            # print('logb shape', img_logb)
             log_likelihood = self.log_likelihood(img, img_mu, img_logb)#[batch, H', W', C]
             log_likelihood = tf.reduce_sum(log_likelihood, axis=[-3,-2,-1])  # [batch]
-            sum_selected_logits = tf.math.reduce_sum(token_samples_onehot * logits, axis=-1)  # [batch*H*W]
+            sum_selected_logits = tf.math.reduce_sum(token_sample_onehot * logits, axis=-1)  # [batch*H*W]
             sum_selected_logits = tf.reshape(sum_selected_logits, [batch, H, W])
             kl_term = tf.reduce_sum(sum_selected_logits, axis=[-2,-1])#[batch]
-            return log_likelihood, kl_term
+            return log_likelihood, kl_term, decoded_img
 
         #num_samples, batch
-        log_likelihood_samples, kl_term_samples = tf.vectorized_map(_single_decode, token_samples_onehot)  # [S, batch], [S, batch]
+        log_likelihood_samples, kl_term_samples, decoded_ims = tf.vectorized_map(_single_decode, token_samples_onehot)  # [S, batch], [S, batch]
+
+        if self.step % 50 == 0:
+            img_mu_0 = tf.reduce_mean(decoded_ims, axis=0)[..., :self.num_channels]
+            img_mu_0 -= tf.reduce_min(img_mu_0)
+            img_mu_0 /= tf.reduce_max(img_mu_0)
+            tf.summary.image('mu', img_mu_0, step=self.step)
+
+            smoothed_img = img[..., self.num_channels:]
+            smoothed_img = (smoothed_img - tf.reduce_min(smoothed_img)) / (
+                    tf.reduce_max(smoothed_img) - tf.reduce_min(smoothed_img))
+            tf.summary.image(f'img_before_autoencoder', smoothed_img, step=self.step)
 
         var_exp = tf.reduce_mean(log_likelihood_samples, axis=0)  # [batch]
         kl_div = tf.reduce_mean(kl_term_samples, axis=0)  # [batch]
-        elbo = var_exp - kl_div #batch
-        loss = - tf.reduce_mean(elbo)#scalar
+        elbo = var_exp - kl_div  # batch
+        loss = - tf.reduce_mean(elbo)  # scalar
 
         entropy = -tf.reduce_sum(logits * tf.math.exp(logits), axis=-1)  # [batch*H*W]
         perplexity = 2. ** (-entropy / tf.math.log(2.))  # [batch*H*W]
         mean_perplexity = tf.reduce_mean(perplexity)  # scalar
+
+        if self.step % 2 == 0:
+            logits = tf.nn.softmax(logits, axis=-1)  # [batch*H*W, num_embedding]
+            logits -= tf.reduce_min(logits)
+            logits /= tf.reduce_max(logits)
+            logits = tf.reshape(logits, [batch, H*W, self.num_embedding])[0]  # [H*W, num_embedding]
+            # tf.repeat(tf.repeat(logits, 16*[4], axis=0), 512*[4], axis=1)
+            tf.summary.image('logits', logits[None, :, :, None], step=self.step)
+            tf.summary.scalar('perplexity', mean_perplexity, step=self.step)
+            tf.summary.scalar('var_exp', tf.reduce_mean(var_exp), step=self.step)
+            tf.summary.scalar('kl_div', tf.reduce_mean(kl_div), step=self.step)
 
 
         return dict(loss=loss,
--- neural_deprojection/models/TwoD_to_2d_dVAE_GCD/main.py
+++ neural_deprojection/models/TwoD_to_2d_dVAE_GCD/main.py
@@ -11,8 +11,19 @@ import glob, os
 import tensorflow as tf
 import json
 import sonnet as snt
+from tensorflow_addons.image import gaussian_filter2d
 
 MODEL_MAP = {'disc_img_vae': DiscreteImageVAE}
+
+@tf.function
+def double_downsample(image):
+    filter = tf.ones((2, 2, 1, 1)) * 0.25
+    img = tf.nn.conv2d(image[None, ...],
+                        filters=filter, strides=2,
+                        padding='SAME')
+    return tf.nn.conv2d(img,
+                        filters=filter, strides=2,
+                        padding='SAME')[0, ...]
 
 def build_training(model_type, model_parameters, optimizer_parameters, loss_parameters, strategy=None, **kwargs) -> TrainOneEpoch:
     model_cls = MODEL_MAP[model_type]
@@ -59,7 +70,9 @@ def build_dataset(tfrecords, batch_size):
                                                              image_shape=(1024, 1024, 1)))  # (graph, image, idx)
 
     dataset = dataset.map(lambda graph_data_dict, img, cluster_idx, projection_idx, vprime:
-                          img).shuffle(buffer_size=50)  # .batch(batch_size=batch_size)
+                          tf.concat([double_downsample(img), gaussian_filter2d(double_downsample(img), filter_shape=[6, 6])], axis=-1))
+
+    dataset = dataset.shuffle(buffer_size=50).batch(batch_size=batch_size)
 
     # dataset = batch_dataset_set_graph_tuples(all_graphs_same_size=True, dataset=dataset, batch_size=batch_size)
 
@@ -76,8 +89,8 @@ def train_disc_img_vae(data_dir, config, kwargs):
     print(f'Number of test tfrecord files : {len(test_tfrecords)}')
     print(f'Total : {len(train_tfrecords) + len(test_tfrecords)}')
 
-    train_dataset = build_dataset(train_tfrecords, batch_size=2)
-    test_dataset = build_dataset(test_tfrecords, batch_size=2)
+    train_dataset = build_dataset(train_tfrecords, batch_size=4)
+    test_dataset = build_dataset(test_tfrecords, batch_size=4)
 
     # with strategy.scope():
     train_one_epoch = build_training(**config, **kwargs)
@@ -86,6 +99,7 @@ def train_disc_img_vae(data_dir, config, kwargs):
     # checkpoint_dir = build_checkpoint_dir('test_checkpointing', config)
     log_dir = 'test_log_dir'
     checkpoint_dir = 'test_checkpointing'
+    save_dir = 'saved_model'
 
     os.makedirs(checkpoint_dir, exist_ok=True)
     with open(os.path.join(checkpoint_dir, 'config.json'), 'w') as f:
@@ -98,23 +112,26 @@ def train_disc_img_vae(data_dir, config, kwargs):
                           early_stop_patience=10,
                           checkpoint_dir=checkpoint_dir,
                           log_dir=log_dir,
-                          debug=False)
+                          save_model_dir=save_dir,
+                          debug=True)
 
 
 def main(data_dir):
     config = dict(model_type='disc_img_vae',
-                  model_parameters=dict(embedding_dim=64,
-                                        num_embedding=512,
-                                        kernel_size=4),
+                  model_parameters=dict(embedding_dim=16,  # 64
+                                        num_embedding=1024,  # 1024
+                                        hidden_size=16,  # 64
+                                        num_token_samples=4,  # 32
+                                        num_channels=1),
                   optimizer_parameters=dict(learning_rate=1e-5, opt_type='adam'),
                   loss_parameters=dict())
-    kwargs = dict(num_token_samples=1)
+    kwargs = dict()
     train_disc_img_vae(data_dir, config, kwargs)
 
 
 if __name__ == '__main__':
-    # tfrec_base_dir = '/home/s2675544/data/tf_records'
-    tfrec_base_dir = '/home/matthijs/Documents/Studie/Master_Astronomy/1st_Research_Project/Data/tf_records'
+    tfrec_base_dir = '/home/s2675544/data/tf_records'
+    # tfrec_base_dir = '/home/matthijs/Documents/Studie/Master_Astronomy/1st_Research_Project/Data/tf_records'
     tfrec_dir = os.path.join(tfrec_base_dir, 'snap_128_tf_records')
 
     main(tfrec_dir)
